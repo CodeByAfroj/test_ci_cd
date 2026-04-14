17a5a91dea2351dab6f850acd01c59dbdc14360d
CodeByAfroj <mulaniafroj26@gmail.com>
2026-04-14 17:02:46 +0530

    Buggy Code

--- src/calc.py
+++ src/calc.py
@@ -4,9 +4,6 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
-
 def divide(a, b):
     if b == 0:
         raise ZeroDivisionError('Cannot divide by zero')
